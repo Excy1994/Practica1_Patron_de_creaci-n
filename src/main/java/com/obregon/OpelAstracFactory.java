d64f04f05e7acf1deac807d1dbca9bba156690aa
Excy1994 <obregondoribel@gmail.com>
2021-10-16 15:07:56 -0600

    clases de coches

--- src/main/java/com/obregon/OpelAstracFactory.java
+++ src/main/java/com/obregon/OpelAstracFactory.java
@@ -4,13 +4,13 @@ public class OpelAstracFactory implements CocheFactory{
 
     @Override
     public Motor crearMotor() {
-        // TODO Auto-generated method stub
+
         return new AstraMotor();
     }
 
     @Override
     public Carroceria crearCarroceria() {
-        // TODO Auto-generated method stub
+   
         return new AstraCarroceria();
     }
     
